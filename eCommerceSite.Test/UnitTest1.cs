ca5d4a7895d20455272f02c678882e533748938e
Logan Grzesiek <lmgrzesiek@gmail.com>
2019-01-23 18:17:15 -0600

    Contact page tweaks (WIP)

---
+++ eCommerceSite.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace eCommerceSite.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
